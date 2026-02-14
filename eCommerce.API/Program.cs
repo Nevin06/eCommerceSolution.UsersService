6da4f650069ffe0a6bb2ec381801b51ec8f82f91
Thomas Nevin K <thomasnevink@gmail.com>
2026-02-14 12:29:55 +0530

    2nd commit

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,10 +1,7 @@
 using eCommerce.API.Middlewares;
 using eCommerce.Core;
 using eCommerce.Core.Mappers;
-using eCommerce.Core.Validators;
 using eCommerce.infrastructure;
-using FluentValidation;
-using System.Reflection;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,18 +18,43 @@ builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializ
 builder.Services.AddAutoMapper(cfg => {}, typeof(ApplicationUserMappingProfile).Assembly);
 builder.Services.AddAutoMapper(cfg => { }, typeof(RegisterRequestMappingProfile).Assembly);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200") // Allow requests from the specified origin (e.g., Angular development server)
+        //.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 //FluentValidations
 //builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 // Build the application, creating a WebApplication instance that will be used to configure and run
 // the web application. This step finalizes the setup of the application's services and middleware
 // based on the configurations specified in the builder.
+
+// Add API explorer services(29)
+builder.Services.AddEndpointsApiExplorer();
+
+// Add Swagger services for API documentation(29)
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
+// Request pipeline configuration
 app.UseExceptionHandlingMiddleware();
 
 // Routing
 app.UseRouting();
+
+app.UseSwagger(); //29
+app.UseSwaggerUI(c =>
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1")
+); //29
+app.UseCors(); // Enable CORS for all origins, methods, and headers. This allows the API to be accessed from any domain, which is useful for development and testing purposes. In production, you may want to configure CORS more restrictively to enhance security by specifying allowed origins, methods, and headers based on your application's requirements.
 
 // Authentication and Authorization Middleware
 app.UseAuthentication();
